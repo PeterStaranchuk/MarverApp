a68e1b3f10b579ad33c6726e3118836485d20c81
Petr Staranchuk <petr.staranchuk@gmail.com>
2019-07-23 17:23:23 +0300

    Created empty tests for characters screen

---
+++ app/src/test/java/com/ps/superheroapp/CharactersScreenTest.kt
@@ -0,0 +1,47 @@
+package com.ps.superheroapp
+
+import org.junit.Test
+
+class CharactersScreenTest {
+
+    @Test
+    fun should_show_list_of_characters_when_screen_started() {
+
+    }
+
+    @Test
+    fun should_show_network_error_when_screen_data_cannot_be_loaded_because_of_internet_connection() {
+
+    }
+
+    @Test
+    fun should_show_general_error_when_screen_data_cannot_be_loaded_because_of_internet_connection() {
+
+    }
+
+    @Test
+    fun should_filter_character_list_when_user_enter_text_in_search_field() {
+
+    }
+
+    @Test
+    fun should_open_detail_information_screen_when_user_clicked_item_in_character_list() {
+
+    }
+
+    @Test
+    fun should_show_progress_bar_when_character_list_loading() {
+
+    }
+
+    @Test
+    fun should_hide_progress_when_network_error_occurred() {
+
+    }
+
+    @Test
+    fun should_hide_progress_when_general_error_occurred() {
+
+    }
+
+}
