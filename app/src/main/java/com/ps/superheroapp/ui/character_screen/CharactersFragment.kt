3412d504385e4a3b8e68ee1dfe982bc3668eba04
Petr Staranchuk <petr.staranchuk@gmail.com>
2019-07-25 10:05:04 +0300

    Add layout for CharacterFragment

--- app/src/main/java/com/ps/superheroapp/ui/character_screen/CharactersFragment.kt
+++ app/src/main/java/com/ps/superheroapp/ui/character_screen/CharactersFragment.kt
@@ -1,6 +1,12 @@
 package com.ps.superheroapp.ui.character_screen
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.ps.superheroapp.databinding.FragmentCharactersBinding
+import com.ps.superheroapp.extentions.getAppComponent
 import javax.inject.Inject
 
 class CharactersFragment : Fragment() {
@@ -8,4 +14,19 @@ class CharactersFragment : Fragment() {
     @Inject
     lateinit var viewModel: CharactersViewModel
 
+    lateinit var binding: FragmentCharactersBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
+            FragmentCharactersBinding.inflate(inflater, container, false).apply {
+                binding = this
+            }.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        getAppComponent()
+                .characterSubcomponent()
+                .with(this)
+                .build()
+                .inject(this)
+    }
 }
