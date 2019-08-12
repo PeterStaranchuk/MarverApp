c0daff359fd448580722048b9cd7132e64c99aa3
Petr Staranchuk <petr.staranchuk@gmail.com>
2019-08-12 10:24:49 +0300

    Split initial load error and next page load errors

--- app/src/main/java/com/ps/superheroapp/objects/CharacterLoadEvent.kt
+++ app/src/main/java/com/ps/superheroapp/objects/CharacterLoadEvent.kt
@@ -1,5 +1,9 @@
 package com.ps.superheroapp.objects
 
 enum class CharacterLoadEvent {
-    INITIAL_LOAD_STARTED, NEXT_PAGE_LOAD_STARTED, LOADED, ERROR
+    INITIAL_LOAD_STARTED,
+    NEXT_PAGE_LOAD_STARTED,
+    LOADED,
+    INITIAL_LOAD_FAILED,
+    NEXT_PAGE_LOAD_FAILED
 }
